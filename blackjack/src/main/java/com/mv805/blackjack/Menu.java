ef51d6d8834b97b492f8ee02c90c1fa14035fe5c
Matt Villa <matthewvilla805@gmail.com>
2022-03-14 23:52:00 -0700

    removed erroneous printlines and improved process method

--- blackjack/src/main/java/com/mv805/blackjack/Menu.java
+++ blackjack/src/main/java/com/mv805/blackjack/Menu.java
@@ -12,21 +12,21 @@ public class Menu {
             System.out.printf("%s: %s%n", Location.getLocation(menuOption).getSelectionString(),
                     Location.getLocation(menuOption).getDisplayName());
         }
-        System.out.println();
+
     }
 
     public static String getMenuInput() {
         System.out.print("Enter Selection:");
         String choice = scanner.next();
-        System.out.println();
         return choice;
     }
 
-    public static void processInput(Player player, String menuChoice) {
+    public static void processMenuInput(Player player, String menuChoice) {
         if (menuChoice.equals(Location.EXIT.getSelectionString())) {
             player.setPlayerState(PlayerState.EXITING);
         } else {
             player.setPlayerLocation(Location.getLocation(menuChoice));
+            player.setPlayerState(PlayerState.MOVING);
         }
     }
 
