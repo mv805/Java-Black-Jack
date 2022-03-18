ae75aff534e363273386f384d979558610f7481a
Matt Villa <matthewvilla805@gmail.com>
2022-03-17 23:17:19 -0700

    Added test to make sure deck is shuffled, fixed name of exception method

--- blackjack/src/test/java/com/mv805/blackjack/CardUtilityTest.java
+++ blackjack/src/test/java/com/mv805/blackjack/CardUtilityTest.java
@@ -1,17 +1,26 @@
 package com.mv805.blackjack;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 
 public class CardUtilityTest {
 
     @Test
-    void testShuffle() {
+    void testShuffleThrowsException() {
         Deck deck = new Deck();
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> CardUtility.shuffle(0, deck.getDeck()));
         assertEquals("Can't shuffle zero times", exception.getMessage());
+    }
+
+    @Test
+    void testShuffleIsShuffled() {
+        Deck deck = new Deck();
+        List checkList = new ArrayList<>(deck.getDeck());
+        CardUtility.shuffle(1, checkList);
+        assertTrue(!deck.getDeck().equals(checkList));
     }
 }
