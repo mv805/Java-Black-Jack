9597a86d8d474e2a23f7f05a581b45cd502fb416
Matt Villa <matthewvilla805@gmail.com>
2022-03-17 23:21:58 -0700

    added player and dealers hand, removed constructor field for playerBet

--- blackjack/src/main/java/com/mv805/blackjack/BlackJackGame.java
+++ blackjack/src/main/java/com/mv805/blackjack/BlackJackGame.java
@@ -1,12 +1,15 @@
 package com.mv805.blackjack;
 
+import java.util.List;
+
 public class BlackJackGame {
 
-    private int playerBet;
+    private int playerBet = 0;
     private BlackJackTable table;
+    private List playersHand;
+    private List dealersHand;
 
     public BlackJackGame(BlackJackTable table) {
-        this.playerBet = 0;
         this.table = table;
     }
 
