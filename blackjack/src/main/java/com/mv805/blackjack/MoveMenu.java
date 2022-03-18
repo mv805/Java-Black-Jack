f8604d92df6a4d9579a53deabc0e40526011047a
Matt Villa <matthewvilla805@gmail.com>
2022-03-17 23:18:12 -0700

    Added logic for displaying tables

--- blackjack/src/main/java/com/mv805/blackjack/MoveMenu.java
+++ blackjack/src/main/java/com/mv805/blackjack/MoveMenu.java
@@ -1,24 +1,43 @@
 package com.mv805.blackjack;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class MoveMenu {
 
     public static Scanner scanner = new Scanner(System.in);
 
-    public static void DisplayMenu(List<String> menuList) {
+    public static void DisplayMenu(Player player) {
 
-        for (String menuOption : menuList) {
-            System.out.printf("%s: %s%n", Location.getLocation(menuOption).getSelectionString(),
-                    Location.getLocation(menuOption).getDisplayName());
+        if (player.getPlayerLocation().equals(Location.TABLES)) {
+            for (HallTable table : HallTable.values()) {
+                System.out.printf("%s: %s-- Min Bet: %d / Max Bet: %d / " +
+                        "Number of Decks used: %d / ",
+                        table.getSelectionString(),
+                        table.getDisplayName(),
+                        table.getTable().getMinBet(),
+                        table.getTable().getMaxBet(),
+                        table.getTable().getNumberOfDecks());
+
+                if (table.getTable().ishitOnSoft17()) {
+                    System.out.printf("Dealer hits on soft 17%n");
+                } else {
+                    System.out.printf("Dealer stands on 17%n");
+                }
+            }
+            System.out.printf("%s: %s%n", Location.HALL.getSelectionString(), Location.HALL.getDisplayName());
+
+        } else {
+            for (String menuOption : player.getPlayerLocation().getMenuList()) {
+                System.out.printf("%s: %s%n", Location.getLocation(menuOption).getSelectionString(),
+                        Location.getLocation(menuOption).getDisplayName());
+            }
         }
 
     }
 
     public static String getMenuInput() {
         System.out.print("Enter Selection:");
-        String choice = scanner.next();
+        String choice = scanner.next().toUpperCase();
         return choice;
     }
 
