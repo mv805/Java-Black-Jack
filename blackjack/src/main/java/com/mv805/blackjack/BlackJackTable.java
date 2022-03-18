7abc1b3667cde4b2c2bd76b44f91cd219b906cd7
Matt Villa <matthewvilla805@gmail.com>
2022-03-17 23:02:32 -0700

    added getters and setters and fixed boolean name

--- blackjack/src/main/java/com/mv805/blackjack/BlackJackTable.java
+++ blackjack/src/main/java/com/mv805/blackjack/BlackJackTable.java
@@ -5,13 +5,29 @@ public class BlackJackTable {
     private int minBet;
     private int maxBet;
     private int numberOfDecks;
-    private boolean soft17hit;
+    private boolean hitOnSoft17;
 
     public BlackJackTable(int minBet, int maxBet, int numberOfDecks, boolean soft17hit) {
         this.minBet = minBet;
         this.maxBet = maxBet;
         this.numberOfDecks = numberOfDecks;
-        this.soft17hit = soft17hit;
+        this.hitOnSoft17 = soft17hit;
+    }
+
+    public int getMinBet() {
+        return minBet;
+    }
+
+    public int getMaxBet() {
+        return maxBet;
+    }
+
+    public int getNumberOfDecks() {
+        return numberOfDecks;
+    }
+
+    public boolean ishitOnSoft17() {
+        return hitOnSoft17;
     }
 
 }
