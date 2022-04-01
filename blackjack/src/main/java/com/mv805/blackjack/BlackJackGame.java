a5135be830d97d92ec2564fdff2045f0aec00e5f
Matt Villa <matthewvilla805@gmail.com>
2022-04-01 13:56:27 -0700

    expanded and main logic started. Shuffle phase added.

--- blackjack/src/main/java/com/mv805/blackjack/BlackJackGame.java
+++ blackjack/src/main/java/com/mv805/blackjack/BlackJackGame.java
@@ -4,18 +4,47 @@ import java.util.List;
 
 public class BlackJackGame {
 
+    //TABLE_1("1", "Table 1", new BlackJackTable(5, 500, 3, false)),
     private int playerBet = 0;
-    private HallTable table;
     private List playersHand;
     private List dealersHand;
+    private HallTable gameTable;
+    private Shoe shoe;
+    private boolean gameOver = false;
+    private boolean cutCardFound = false;
+    private final int SHUFFLE_CYCLES = 20;
+    private BlackJackGamePhase gamePhase = BlackJackGamePhase.SHUFFLE;
 
     public BlackJackGame(HallTable table) {
-        this.table = table;
-        mainGameLogic(table);
+        this.gameTable = table;
+        //this.minBet = table.getTable().getMinBet();
+        //this.maxBet = table.getTable().getMaxBet();
+        //this.numberOfDecks = table.getTable().getNumberOfDecks();
+        //this.soft17hit = table.getTable().ishitOnSoft17();
+        shoe = new Shoe(gameTable.getTable().getNumberOfDecks());
+
+        MainGameLogic(table);
     }
 
-    public static void mainGameLogic(HallTable table){
-        System.out.println("in the game with table " + table.getDisplayName());
+    private void MainGameLogic(HallTable table){
+        while (!gameOver) {
+            switch (gamePhase) {
+                case SHUFFLE:
+                    ShufflePhase();
+                    break;
+                case BETTING:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void ShufflePhase(){
+        if (cutCardFound){
+            shoe = new Shoe(gameTable.getTable().getNumberOfDecks());
+            System.out.println("Cut card issued last round. New Shoe is being prepared...");
+        } 
     }
 
 }
